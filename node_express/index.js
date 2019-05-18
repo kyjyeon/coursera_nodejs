1e48d12f2655de6d951169a4fe64c1732b1c41b2
kyjyeon <kyjyeon93@gmail.com>
2019-05-18 20:36:22 +0900

    new Routers(promotion, leader) added

--- node_express/index.js
+++ node_express/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname =  'localhost';
 PORT = 3000;
 
@@ -10,55 +13,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req,res,next)=>{
-    //no matter what request kind comes(GET, POST, CREATE etc) this will be executed
-    res.statusCode = 200;
-    res.setHeader('Context-Type', 'text/plain');
-    next();//it'll continue on to look for additional specifications down below here, which will match this dishes endpoint. So this would be done for all the requests, get, put, post, and delete, on the dishes, and it'll continue on to the next one.  
-})
-
-app.get('/dishes', (req,res,next)=>{
-    //Because of app.all next(), this will be called.
-    res.end('Will send all the dishes to customer');
-})
-
-app.post('/dishes',(req,res,next)=>{
-    res.end('Will add info to dish: ' + req.body.name + 
-    ' with details: '+ req.body.description);
-})
-
-app.put('/dishes', (req, res, next)=>{
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-})
-
-app.delete('/dishes', (req, res, next)=>{
-    //Dangerous operation so should make operation that only admins can get the access
-    res.end('Deleting all the dishes!');
-})
-
-app.get('/dishes/:dishID', (req,res,next)=>{
-    //Because of app.all next(), this will be called.
-    res.end('Will send the details of Selected dish: ' 
-    + req.params.dishID);
-})
-
-app.post('/dishes/:dishID',(req,res,next)=>{
-    res.statusCode = 403;
-    res.end('POST operation not supported on: /dishes/'
-    + req.params.dishID);
-})
-
-app.put('/dishes/:dishID', (req, res, next)=>{
-    res.write('Updating the dish ' + req.params.dishID + '\n');
-    res.end('Will update the dish: ' + req.body.name
-    + ' with detail: ' + req.body.description);
-})
-
-app.delete('/dishes/:dishID', (req, res, next)=>{
-    //Dangerous operation so should make operation that only admins can get the access
-    res.end('Deleting dish: ' + req.params.dishID);
-})
+//Anything coming from /dishes are handled by dishRouter
+app.use('/dishes', dishRouter);
+app.use('/dishes/:dishID', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/promotions/:promoID', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/leaders/:leaderid', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'))
 
