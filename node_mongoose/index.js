260d605580ff43ce0d69be8b192257727a2b8e12
kyjyeon <kyjyeon93@gmail.com>
2019-05-21 16:16:32 +0900

    Using mongoose to make structured data with mongodb

---
+++ node_mongoose/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then(()=>{
+    console.log("Connected to the mongodb database server...");
+
+    var newDish = Dishes({
+        name: 'Chicken',
+        description: "Fried Chicken"
+    });
+
+    newDish.save()
+        .then((dish)=>{
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes)=>{
+            console.log(dishes);
+
+            return Dishes.remove({});
+        })
+        .then((result)=>{
+            console.log(result);
+            return mongoose.connection.close();
+        })
+        .catch((err)=>{
+            console.log(err);
+        });
+});
