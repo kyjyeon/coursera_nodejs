4773c048b0973574e91d5d06bbaed1a1d1b577d3
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-07-25 02:08:39 +0900

    Create async_await.js

---
+++ examples/async_await.js
@@ -0,0 +1,14 @@
+async function fetchAuthorName(postId) {
+    const postResponse = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+    const post = await postResponse.json();
+    const userId = post.userId;
+    
+    try {
+        const userResponse = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        const user = await userResponse.json();
+        return user.name;
+    } catch (err) {
+        console.log('Faile to fetch user:', err);
+        return "Unknown";
+    }
+}
